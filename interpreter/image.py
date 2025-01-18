a680bbc8814ab0705e556f9e3193f351571f9c2f
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2025-01-18 13:51:42 +0800

    add type hinting

--- interpreter/image.py
+++ interpreter/image.py
@@ -1,5 +1,7 @@
 import pdfminer
+import typing
 from pdfminer.image import ImageWriter
+
 def writeImage(image: pdfminer.layout.LTImage, outputDir: str):
     iw = ImageWriter(outputDir)
     iw.export_image(image)
@@ -8,7 +10,13 @@ def isImage(layout_object):
     return isinstance(layout_object, pdfminer.layout.LTImage) or isinstance(layout_object, pdfminer.layout.LTImage)
 
 
-def Image(layout_object: pdfminer.layout.LTImage, pageNumber: int):
+class Image(typing.TypedDict):
+    content: pdfminer.layout.LTImage
+    coordinates: tuple[float] # follows layout_object.bbox
+    pageNumber: int
+    
+    
+def Image(layout_object: pdfminer.layout.LTImage, pageNumber: int) -> Image:
     return {
         "content": layout_object, # store as pdfminer.layout.LTImage
         "coordinates": layout_object.bbox,
--- interpreter/text.py
+++ interpreter/text.py
@@ -1,18 +1,17 @@
 import pdfminer
-'''
-text: Text = {
-    "content": str,
-    "coordinates": layout_object.bbox
-    "pageNumber": int 
-}
-'''
+import typing
+
+class Text(typing.TypedDict):
+    content: str
+    coordinates: tuple[float] # follows layout_object.bbox
+    pageNumber: int
 
 
 def isText(layout_object):
     return isinstance(layout_object, pdfminer.layout.LTTextBox)
 
 
-def Text(layout_object: pdfminer.layout.LTTextBox, pageNumber: int):
+def Text(layout_object: pdfminer.layout.LTTextBox, pageNumber: int) -> Text:
     return {
         "content": layout_object.get_text().strip().replace("\n",""),
         "coordinates": layout_object.bbox,
