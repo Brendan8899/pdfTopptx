81f8e284c31adf7614670aae92cece4d575163ae
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2025-01-18 12:48:23 +0800

    add image extracter

--- compile/text.py
+++ compile/text.py
@@ -3,8 +3,34 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
+import pdfminer
+from pdfminer.image import ImageWriter
+from pdfminer.high_level import extract_pages
+import os
 
-def compileText(inputFile: str):
+def ensureDir(outputDir):
+    if not os.path.exists(outputDir): 
+        os.makedirs(outputDir) 
+
+def getImage(layout_object):
+    if isinstance(layout_object, pdfminer.layout.LTImage):
+        return layout_object
+    if isinstance(layout_object, pdfminer.layout.LTContainer):
+        for child in layout_object:
+            return getImage(child)
+    else:
+        return None
+
+
+
+def compileImage(page: pdfminer.layout.LTPage, outputDir: str):
+    ensureDir(outputDir)  
+    images = list(filter(bool, map(getImage, page)))
+    iw = ImageWriter(outputDir)
+    for image in images:
+        iw.export_image(image)
+
+def compile(inputFile: str):
     fp = open(inputFile, 'rb')
     rsrcmgr = PDFResourceManager()
     laparams = LAParams()
@@ -16,13 +42,15 @@ def compileText(inputFile: str):
         print('Processing next page...')
         interpreter.process_page(page)
         layout = device.get_result()
+        compileImage(layout, "output")
+        '''
         for lobj in layout:
-            print(type(lobj).__name__)
             if isinstance(lobj, LTFigure):
-                print(dir(LTFigure))
-            '''
+                extractedFigure = getImage(lobj)
+                print(extractedFigure)
+
             if isinstance(lobj, LTTextBox):
                 x, y, text = lobj.bbox[0], lobj.bbox[3], lobj.get_text()
                 print('At %r is text: %s' % ((x, y), text))
             '''
-compileText("test/P5 CW Quesitons.pdf")
+compile("test/P5 CW Quesitons.pdf")
